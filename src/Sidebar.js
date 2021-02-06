cbd993be85bfda5c64789daf3099edfff7cab218
Gaurav Singh <76627244+Thatsgaurav@users.noreply.github.com>
2021-02-06 12:24:15 +0530

    commmit

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,10 +1,31 @@
+import { Avatar } from '@material-ui/core'
 import React from 'react'
 import './Sidebar.css'
 
 function Sidebar() {
     return (
         <div className='sidebar'>
-            <h1>I am a sidebar!</h1>
+            <div className="sidebar__top">
+                <img src="https://images.unsplash.com/photo-1579546929518-9e396f3cc809?ixlib=rb-1.2.1&i
+                          ixid=MXwxMjA3fdB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHw%3D&w=1000&q=80" alt="" />
+                <Avatar className="sidebar__avatar"/>
+                <h2>Gaurav Singh</h2>
+                <h4>Togaurav20@gmail.com</h4>
+            </div>
+
+            <div className="sidebar__stats">
+                <div className="sidebar__stat">
+                    <p>Who viewed you</p>
+                    <p className="sidebar__statNumber">100</p>
+                </div>
+                <div className="sidebar__stat">
+                    <p>Views on post</p>
+                    <p className="sidebar__statNumber">115</p>
+                </div>
+            </div>
+            <div className="sidebar__bottom">
+                <p>Recent</p>
+            </div>
         </div>
     );
 }
