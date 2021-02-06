9c18ccda79ce6e1234a3cf055269fe4d721742db
Gaurav Singh <76627244+Thatsgaurav@users.noreply.github.com>
2021-02-06 19:02:20 +0530

    commit

--- src/Feed.js
+++ src/Feed.js
@@ -1,13 +1,25 @@
 import React from 'react'
-import './Feed.css'
+import "./Feed.css"
+import CreateIcon from '@material-ui/icons/Create';
+
 
 function Feed() {
     return (
-        <div className="feed">
-            <div className="feed__inputContainer" />
-            
+        <div className="Feed">
+            <div className="feed__inputContainer">
+                <div className="feed__input">
+                    <CreateIcon />
+                    <form>
+                        <input type="text" />
+                        <button type='submit'>Send</button>
+                    </form>
+                </div>
+            </div>
         </div>
     )
 }
 
 export default Feed
+
+
+
