8c6b7cedada6bbeb1c40b06ec626d7b41596e9fb
Gaurav Singh <76627244+Thatsgaurav@users.noreply.github.com>
2021-02-06 11:32:46 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import './App.css';
 import Header from './features/counter/Header';
+import Sidebar from './Sidebar';
 
 function App() {
   return (
     <div className="App">
-
-      {/* Header */}
       <Header />
 
       {/* App Body */}
+      <div className="app__body">
+        <Sidebar />
       {/* Sidebar */}
       {/* Feed */}
       {/* Widgets */}
+      </div>
     </div>
   );
 }
