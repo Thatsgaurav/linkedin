b7e524ef12eb7f2dda3a3cf7350d07f4a95170d5
Gaurav Singh <76627244+Thatsgaurav@users.noreply.github.com>
2021-02-08 00:54:52 +0530

    commit

---
+++ src/Widgets.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import './Widgets.css';
+import InfoIcon from '@material-ui/icons/Info';
+import FiberManualRecordIcon from '@material-ui/icons/FiberManualRecord';
+
+function Widgets() {
+  const newsArticle = (heading, subtitle) => (
+    <div className="widget_article">
+      <div className="widgets_articleleft">
+        <FiberManualRecordIcon />
+      </div>
+
+      <div className="widgets_articleright">
+        <h4>{heading}</h4>
+        <p>{subtitle}</p>
+      </div>
+    </div>
+  );
+
+  return (
+    <div className="widgets">
+      <div className="widget_header">
+        <h2>LinkedIn News</h2>
+        <InfoIcon />
+      </div>
+
+      {newsArticle("Meet Amazon's new boss", "3d ago • 48,396 readers")}
+      {newsArticle("Massive NRI rush to buy homes", "3d ago • 8,886 readers")}
+      {newsArticle("Is WFH more stressful?", "3d ago • 6,910 readers")}
+    </div>
+  )
+}
+
+export default Widgets
