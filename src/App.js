28227f500ccc80f14f6f86c609d0d64c3b371a3f
Gaurav Singh <76627244+Thatsgaurav@users.noreply.github.com>
2021-02-07 20:36:25 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
-import React from 'react';
-import { useSelector } from "react-redux"
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from "react-redux"
 import './App.css';
-import Header from './Header';
+import { login, logout, selectUser } from './features/userSlice';
 import Feed from './Feed';
-import Sidebar from './Sidebar';
-import { selectUser } from './features/userSlice';
+import { auth } from "./firebase"
+import Header from './Header';
 import Login from './Login';
+import Sidebar from './Sidebar';
 
 function App() {
   const user = useSelector(selectUser)
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    auth.onAuthStateChanged(userAuth => {
+      if (userAuth) {
+        // user is logged in
+        dispatch(login({
+          email: userAuth.email,
+          uid: userAuth.uid,
+          displayName: userAuth.displayName,
+          photoUrl: userAuth.photoURL,
+        }))
+      } else {
+        // use is logged out
+        dispatch(logout());
+      }
+    })
+  }, [dispatch]);
 
   return (
     <div className="App">
--- src/Login.js
+++ src/Login.js
@@ -1,24 +1,58 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useDispatch } from 'react-redux';
+import { login } from './features/userSlice';
+import { auth } from './firebase';
 import './Login.css';
 
 function Login() {
-    const LoginToApp = () => {};
 
-    const register = () => {};
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [name, setName] = useState("");
+    const [profilePic, setProfilePic] = useState("");
+    const dispatch = useDispatch();
+
+    const LoginToApp = (e) => {
+        e.preventDefault()
+    };
+
+    const register = () => {
+        if (!name) {
+            return alert('Please enter a full name!')
+        }
+
+        auth.createUserWithEmailAndPassword(email, password)
+        .then((userAuth) => {
+            userAuth.user.updateProfile({
+                displayName: name,
+                photoURL: profilePic,
+            })
+            .then(() => {
+                dispatch(login({
+                    email: userAuth.user.email,
+                    uid: userAuth.user.uid,
+                    displayName: name,
+                    photoUrl: profilePic,
+                })
+              );
+            })
+        })
+        .catch(error => alert(error));
+    };
 
     return (
         <div className="Login">
             <img src="https://news.hitb.org/sites/default/files/styles/large/public/field/image/500px-LinkedIn_Logo.svg__1.png?itok=q_lr0Vks" alt="" />
 
             <form>
-                <input placeholder="Full name (required if registering)"
+                <input value={name} onChange={e => setName(e.target.value)} placeholder="Full name (required if registering)"
                 type="text" />
 
-                <input placeholder='Profile pic URL (optional)' type="text" />
+                <input value={profilePic} onChange={e => setProfilePic(e.target.value)} placeholder='Profile pic URL (optional)' type="text" />
 
-                <input placeholder='Email' type="email" />
+                <input value={email} onChange={e => setEmail(e.target.value)} placeholder='Email' type="email" />
 
-                <input placeholder='Password' type="password" />
+                <input value={password} onChange={e => setPassword(e.target.value)} placeholder='Password' type="password" />
 
                 <button type='submit' onClick={LoginToApp}>Sign In</button>
             </form>
